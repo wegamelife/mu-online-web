b88b089dfd11a99f2f347f0916f530887760d05e
buuug7 <youpp@126.com>
2021-08-12 01:24:13 +0800

    update

--- client/pages/api/getByUserName.js
+++
@@ -1,7 +0,0 @@
-import {getByUserName} from "../../lib";
-
-export default async function handler(req, res) {
-    const {userName} = req.params;
-    const user = await getByUserName(userName);
-    res.json(user);
-}
